4027707137e589edafddcdec344a2f42280e6e32
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-01 16:33:14 -0800

    finished sql practice

--- sql-practice/phase-4.sql
+++ sql-practice/phase-4.sql
@@ -2,6 +2,7 @@
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS relationships;
 DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS parties;
 
 CREATE TABLE employees (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -68,9 +69,89 @@ VALUES
 -- p12
 ("Jim Halpert", 4.2);
 -- p13
-UPDATE reviews SET score = 9.0 WHERE name="Dwite Schrute";
+UPDATE reviews SET score = 9.0 WHERE employee="Dwite Schrute";
 -- p14
-UPDATE reviews SET score = 9.3 WHERE name="Jim Halpert";
+UPDATE reviews SET score = 9.3 WHERE employee="Jim Halpert";
 
 -- p15
 UPDATE employees SET role = "Assistant Regional Manager" WHERE first_name="Jim" AND last_name="Halpert";
+
+-- p16
+UPDATE employees SET department = 'Sales' WHERE first_name='Ryan' AND last_name='Howard';
+UPDATE employees SET role = 'Sales Representative' WHERE first_name='Ryan' AND last_name='Howard';
+
+-- p17
+INSERT INTO parties (budget, onsite)
+VALUES
+  (200.00, 1);
+
+-- p18
+INSERT INTO relationships (employee1, employee2)
+VALUES
+  ('Angela Martin', 'Dwight Scrute');
+
+-- p19
+INSERT INTO reviews (employee, score)
+VALUES
+  ('Angela Martin', 6.2);
+
+-- p20
+INSERT INTO relationships (employee1, employee2)
+VALUES
+  ('Ryan Howard', 'Kelly Kapoor');
+
+-- p21
+INSERT INTO parties (budget, onsite)
+VALUES
+  (50.00, 1);
+
+-- p22
+DELETE FROM employees WHERE first_name='Jim' AND last_name='Halpert';
+DELETE FROM relationships WHERE employee1='Jim Halpert' OR employee2='Jim Halpert';
+DELETE FROM reviews WHERE employee='Jim Halpert';
+
+-- p23
+DELETE FROM relationships WHERE employee1= 'Roy Anderson' OR employee2='Roy Anderson';
+
+-- p24
+INSERT INTO reviews (employee, score)
+VALUES
+  ('Pam Beesly', 7.6);
+
+-- p25
+INSERT INTO reviews (employee, score)
+VALUES
+  ('Dwight Schrute', 8.7);
+
+-- p26
+DELETE FROM employees WHERE first_name='Ryan' AND last_name='Howard';
+DELETE FROM relationships WHERE employee1='Ryan Howard' OR employee2='Ryan Howard';
+DELETE FROM reviews WHERE employee='Ryan Howard';
+
+-- p27
+INSERT INTO employees (first_name, last_name, department, role)
+VALUES
+  ('Jim', 'Halpert', 'Sales', 'Sales Representative'),
+-- p28
+  ('Karen', 'Filippelli', 'Sales', 'Sales Representative');
+
+-- p29
+INSERT INTO relationships (employee1, employee2)
+VALUES
+  ('Jim Halpert', 'Karen Filippelli');
+
+-- p30
+INSERT INTO parties (budget, onsite)
+VALUES
+  (120.00, 1);
+
+-- p31
+DELETE FROM parties WHERE budget = 120.00;
+INSERT INTO parties (budget, onsite)
+VALUES
+  (300.00, 0);
+
+-- p32
+
+
+-- p33
